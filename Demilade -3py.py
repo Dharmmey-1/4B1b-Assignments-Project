09e4df165c5ac48314ee47cc003e2af9f2084eaa
Demilade somide <demisomide@gmail.com>
2021-03-05 12:04:47 +0100

    Update Demilade -3py.py

--- Demilade -3py.py
+++ Demilade -3py.py
@@ -1,7 +1,10 @@
+# A program that checks if there are common values from two seperate user input
+
 user_data_1 = input("Enter data: ").lower()
 user_data_2 = input("Enter data: ").lower()
 white_space = user_data_1.find(" ")
 white_space2 = user_data_2.find(" ")
+
 
 if (white_space == -1) and (white_space2 == -1):
     if user_data_1 == user_data_2:
